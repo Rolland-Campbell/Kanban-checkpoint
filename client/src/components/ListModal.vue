de510c9d9e00a8f32013fb0d7e50494c3f5e8dbd
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-17 14:29:36 -0600

    service: body not required modal = removed bodyinput

--- client/src/components/ListModal.vue
+++ client/src/components/ListModal.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="create-list-modal" class="modal" tabindex="-1" role="dialog">
-    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -19,17 +18,6 @@
                 id="title"
                 placeholder="Enter a title"
                 v-model="newList.title"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <label for="body">Body</label>
-              <input
-                type="text"
-                class="form-control"
-                id="body"
-                placeholder="Enter some stuff"
-                v-model="newList.body"
                 required
               />
             </div>
