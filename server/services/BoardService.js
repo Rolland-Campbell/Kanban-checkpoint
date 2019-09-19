11c5e91fd751d8117c95b8efeb2c3e5159cbaa28
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-19 16:40:16 -0600

    ignored conditions errors

--- server/services/BoardService.js
+++ server/services/BoardService.js
@@ -20,8 +20,11 @@ let _schema = new Schema({
 _schema.pre('findOneAndRemove', function (next) {
   //lets find all the lists and remove them
   Promise.all([
+    // @ts-ignore
     _listRepo.deleteMany({ boardId: this._conditions._id }),
+    // @ts-ignore
     _taskRepo.deleteMany({ boardId: this._conditions._id }),
+    // @ts-ignore
     _commentRepo.deleteMany({ boardId: this._conditions._id })
   ])
     .then(() => next())
